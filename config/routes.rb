8fdd435d690b7c379d93a9a616255f18919fcab1
@nanami-skrm <skrm773@gmail.com>
2020-08-22 23:35:54 +0900

    [Finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
+	get 'todolists/new'
 	get 'top' => 'homes#top'
 	post 'todolists' => 'todolists#create'
+	get 'todolists' => 'todolists#index'
+	get 'todolists/:id' => 'todolists#show', as: 'todolist'
 end
