b7815610b7e09b492fc9ef92d18b62cc8aa46a67
@nanami-skrm <skrm773@gmail.com>
2020-08-23 14:58:36 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6aa86c5173962c7fe5cacc051c2d6184b31e29d74691b4d37e1830a816d00390b36ba1315292ed09ed0e923a5f12f59fd8c80771e4c28ddb0429349dc8030739'
